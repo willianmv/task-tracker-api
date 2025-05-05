df5838876c9cd268696e02a961f03feebeb5f1fe
willianmv <willianvmarques300@gmail.com>
2025-05-05 18:07:39 -0300

    feat: implementa documentação simples com springdoc-openapi

--- src/main/java/com/example/tasks/controllers/TaskController.java
+++ src/main/java/com/example/tasks/controllers/TaskController.java
@@ -4,6 +4,10 @@ import com.example.tasks.domain.dtos.TaskDto;
 import com.example.tasks.domain.entities.Task;
 import com.example.tasks.mappers.TaskMapper;
 import com.example.tasks.services.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +24,7 @@ public class TaskController {
 
     private final TaskMapper taskMapper;
 
+    @Operation(summary = "Retorna todas as tarefas associadas à lista especificada")
     @GetMapping
     public List<TaskDto> listTasks(@PathVariable("taskListId")UUID taskListId){
         return this.taskService.listTasks(taskListId)
@@ -28,17 +33,20 @@ public class TaskController {
                 .toList();
     }
 
+    @Operation(summary = "Retorna uma tarefa específica associada à lista fornecida" )
     @GetMapping("/{taskId}")
     public Optional<TaskDto> getTask(@PathVariable("taskListId")UUID taskListId, @PathVariable("taskId") UUID taskId){
         return this.taskService.getTask(taskListId, taskId).map(this.taskMapper::toDto);
     }
 
+    @Operation(summary = "Cria uma tarefa e a associa à lista especificada" )
     @PostMapping
     public TaskDto createTask(@PathVariable("taskListId") UUID taskListId, @RequestBody TaskDto dto){
         Task task = this.taskService.createTask(taskListId, this.taskMapper.fromDto(dto));
         return this.taskMapper.toDto(task);
     }
 
+    @Operation(summary = "Localiza a tarefa na lista fornecida e realiza a atualização")
     @PutMapping("/{taskId}")
     public TaskDto updateTask(
             @PathVariable("taskListId")UUID taskListId,
@@ -47,6 +55,7 @@ public class TaskController {
         return this.taskMapper.toDto(updatedTask);
     }
 
+    @Operation(summary = "Localiza a tarefa na lista fornecida e a exclui")
     @DeleteMapping("/{taskId}")
     public void deleteTask(@PathVariable("taskListId")UUID taskListId, @PathVariable("taskId") UUID taskId){
         this.taskService.deleteTask(taskListId, taskId);
--- src/main/java/com/example/tasks/controllers/TaskListController.java
+++ src/main/java/com/example/tasks/controllers/TaskListController.java
@@ -4,6 +4,7 @@ import com.example.tasks.domain.dtos.TaskListDto;
 import com.example.tasks.domain.entities.TaskList;
 import com.example.tasks.mappers.TaskListMapper;
 import com.example.tasks.services.TaskListService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +21,7 @@ public class TaskListController {
 
     private final TaskListMapper taskListMapper;
 
+    @Operation(summary = "Recupera todas as listas de tarefas")
     @GetMapping
     public List<TaskListDto> listTaskLists(){
         return taskListService.listTaskLists()
@@ -28,23 +30,27 @@ public class TaskListController {
                 .toList();
     }
 
+    @Operation(summary = "Recupera uma lista de tarefas específica")
     @GetMapping("/{taskListId}")
     public Optional<TaskListDto> getTaskList(@PathVariable("taskListId")UUID id){
         return this.taskListService.getTaskList(id).map(this.taskListMapper::toDto);
     }
 
+    @Operation(summary = "Cria uma lista de tarefas com os dados fornecidos")
     @PostMapping
     public TaskListDto crateTaskList(@RequestBody TaskListDto dto){
         TaskList taskList = taskListService.createTaskList(this.taskListMapper.fromDto(dto));
         return this.taskListMapper.toDto(taskList);
     }
 
+    @Operation(summary = "Atualiza uma lista de tarefas existente")
     @PutMapping("/{taskListId}")
     public TaskListDto updateTaskList(@PathVariable("taskListId")UUID id, @RequestBody TaskListDto dto){
         TaskList updatedTask = taskListService.updateTaskList(id, this.taskListMapper.fromDto(dto));
         return this.taskListMapper.toDto(updatedTask);
     }
 
+    @Operation(summary = "Exclui uma lista de tarefas específica")
     @DeleteMapping("/{taskListId}")
     public void deleteTaskList(@PathVariable("taskListId")UUID id){
         this.taskListService.deleteTaskList(id);
